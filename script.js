0c82f630883ce8449d5aba3082dc2d71fc2678cd
garik-star1 <76990924+garik-star1@users.noreply.github.com>
2021-01-05 11:14:14 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict"
+
+/*$(function () {
+    $('.menusm').click((event) => {
+        $('.dropdown').toggleClass('show-drop')
+    })
+    $('.add').click((e) => {
+        let v = $("input").val();
+        console.log(v);
+        $('.dropdown li:last-child').after('<li><a href="">' + v + '</a></li>')
+    })
+
+   
+
+
+})
+
+
+$(function () {
+    $('.open').on('click', ()=>{
+        $('.popup').addClass('show')
+    })
+    $('.close').on('click',()=>{
+        $('.popup').removeClass('show')
+    })
+   $('.switch').on('click', ()=>{
+        $('.switch').toggleClass('active');
+        $('.varac').toggleClass('z')
+    })
+})
+$('.sub').on('click',()=> {
+    if($(".input").val()===""){
+        $(alert('dashtery datark en'))  ;
+    }
+})*/
